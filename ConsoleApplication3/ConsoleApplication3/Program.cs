1e29254344bf8cb93c558e55052e4865950b0a85
CaliforniaK15 <mymyke71@gmail.com>
2017-03-05 15:25:20 -0800

    3rd try

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+namespace ConsoleApplication3
+{
+    public abstract class sample
+    {
+        public abstract void donothing();
+    }
+
+    internal class subsample : sample
+    {
+        public override void donothing()
+        {
+            WriteLine("I got abstracted");
+        }
+    }
+
+    internal sealed class myseal
+    {
+        public int mon { get; set; }
+    }
+
+    internal static class mmmm{
+
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            subsample sam = new subsample();
+
+            myseal ny = new myseal();
+            
+            sam.donothing();
+            Read();
+        }
+    }
+}
